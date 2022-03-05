c59a60f7e3ac2f2a5f49e1fb5dabf48931f0196d
Matthew Tramantano <matt.tramantano@gmail.com>
2022-03-04 19:35:24 -0700

    updated file

--- js/functions.ts
+++ js/functions.ts
@@ -12,3 +12,34 @@ function myFunction(someVar: string) {
 const somefunk = (someVar: string) => {
     console.log("Somefunk printed: " + someVar)
 }
+
+const someSimpleFunk = () => "k";
+
+// objects
+type Jose = {analEndurance: number, dickSuckingCap: number, arosal: number}
+const jose: Jose = {
+    analEndurance: 10,
+    dickSuckingCap: 100,
+    arosal: 10,
+}
+function evalGayness(aJose: Jose): boolean {
+    const gayThreshold = 100;
+    let gayLevel = 0;
+    gayLevel = aJose.analEndurance + aJose.dickSuckingCap + aJose.arosal
+    return gayLevel > gayThreshold
+}
+
+console.log("Is Jose Gay? " + evalGayness(jose));
+
+function cleanString(str: string): string {
+    return str.trim(); //"    fjdslkfajl        " | "fjdslkfajl"
+}
+
+function makeElement(str: string): string {
+    return `<${str}></${str}>`;
+}
+
+let someString = "   table    "
+const eleMaker = (str: string) => makeElement(cleanString(str));
+const ele = eleMaker(someString);
+console.log(ele);
