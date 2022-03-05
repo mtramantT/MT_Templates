920419950b203bede740ff424ca2d360433809ba
Matthew Tramantano <matt.tramantano@gmail.com>
2022-03-04 18:49:01 -0700

    added function tut

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+// Variable Declaration Types
+var varVariable: string = "var-variable";
+let letVariable: string = "let-variable"
+const constVariable: string = "const-variable"
+
+// Named function
+function myFunction(someVar: string) {
+    console.log("Somefunk printed: " + someVar)
+}
+
+// Lambda function
+const somefunk = (someVar: string) => {
+    console.log("Somefunk printed: " + someVar)
+}
